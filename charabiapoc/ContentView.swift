e4f36fb75d22d70a3756d9c334260e3e6732bdd8
Ibrahima Ciss <bionik6@gmail.com>
2022-06-09 15:15:23 +0000

    Initial Commit

---
+++ charabiapoc/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  charabiapoc
+//
+//  Created by Ibrahima Ciss on 09/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ charabiapoc/charabiapocApp.swift
@@ -0,0 +1,17 @@
+//
+//  charabiapocApp.swift
+//  charabiapoc
+//
+//  Created by Ibrahima Ciss on 09/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct charabiapocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
